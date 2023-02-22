1dd69d556b06493eba34eaa651cfaf54ae4cf4cc
edicybereye <edi.cybereye@gmail.com>
2023-02-22 13:58:20 +0700

    revisi

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express()
 
 app.listen(5001, () => console.log('API Running on Port 5001'))
 
-app.get('/', (req, res) => res.json('MY API Running'))
+app.get('/', (req, res) => res.json('MY API Running on Port 5001'))
